39901ce53a9f1a441e8146bb90251845948efd19
valsimplon <val.simplon@gmail.com>
2017-03-08 17:08:51 +0100

    encore un test ok (h min sec)

--- src/main/java/co/simplon/dates/DecouverteDates.java
+++ src/main/java/co/simplon/dates/DecouverteDates.java
@@ -12,20 +12,20 @@ public class DecouverteDates {
 	}
 
 	public boolean estInferieurDateCourante(Date date) throws ParseException {
-		
+
 		Boolean retour = false;
 		Date aujourdhui = new Date();
 		if (date.before(aujourdhui)) {
 			retour = true;
 		} else if (date.after(aujourdhui)) {
-			retour =  false;
+			retour = false;
 		}
-		
+
 		return retour;
 	}
 
 	public Date construireDate(String chaineFournie) {
-		
+
 		SimpleDateFormat formatter1 = new SimpleDateFormat("dd/MM/yyyy");
 		try {
 			Date dateFinale = formatter1.parse(chaineFournie);
@@ -34,12 +34,12 @@ public class DecouverteDates {
 			erreur.printStackTrace();
 		}
 		return null;
-		
+
 	}
 
 	public Date augmenterDate(Date dateInitiale, int nombreJours,
 			int nombreHeures, int nombreMinutes) {
-		
+
 		Calendar calendrier = Calendar.getInstance();
 		calendrier.setTime(dateInitiale);
 		calendrier.add(Calendar.DATE, nombreJours);
@@ -50,16 +50,23 @@ public class DecouverteDates {
 	}
 
 	public String formaterUneDate(Date date) {
-		
+
 		SimpleDateFormat dateFormatte = new SimpleDateFormat("dd_MM_yyyy");
 		return dateFormatte.format(date);
 	}
 
 	public String formaterUneHeure(Date time) {
-		// TODO
-		return "";
+
+		SimpleDateFormat heureFormatte = new SimpleDateFormat("HHmmss");
+		String heureFinale = heureFormatte.format(time).substring(0, 2) + "h"
+				+ heureFormatte.format(time).substring(2, 4) + "min"
+				+ heureFormatte.format(time).substring(4, 6) + "sec";
+
+		return heureFinale;
+		
+//		return heureFormatte.format(time);
 	}
-	
+
 	public String formaterDateEnFrancais(Date date) {
 		// TODO
 		return "";
